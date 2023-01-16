fdd1ea196ef4ad65ef7be811668b0b30ea32a597
Amine Neggazi <neggazimedlamine@gmail.com>
2023-01-16 10:14:00 +0100

    updated sort.c

--- serie3/sort/sort.c
+++ serie3/sort/sort.c
@@ -6,16 +6,19 @@ int
 main() {
   int arr[] = { -1, 9, 2, 3, 5, 8 };
 
-  for (int i = 0, temp; i < N - 1; i++)
-    for (int j = i + 1; j < N; j++)
+  for (int i = 0, temp; i < N - 1; i++) {
+    for (int j = i + 1; j < N; j++) {
       if (arr[j] < arr[i]) {
         temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
       }
+    }
+  }
 
   printf("Sorted array = {");
-  for (int i = 0; i < N; i++)
+  for (int i = 0; i < N; i++) {
     printf(" %d,", arr[i]);
+  }
   printf(" }\n");
 }
